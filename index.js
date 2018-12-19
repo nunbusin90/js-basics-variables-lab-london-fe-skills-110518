68c35a41946483dfcdc1c2b377e3632134f6625c
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-19 18:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Code your solution in this file!
-let companyName = 'Scuber';
+// // Code your solution in this file!
+// let companyName = 'Scuber';
 
 const companyName = 'specialCompany'
   function typeError() {
