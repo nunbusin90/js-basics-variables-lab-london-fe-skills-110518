1a4b6e408f923ec2b16f3db187059a36493a4e6d
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-19 18:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
-  function Scuber() {
+  function typeError() {
     if (companyName != ('Scuber')) {
       throw ("TypeError");
     }
