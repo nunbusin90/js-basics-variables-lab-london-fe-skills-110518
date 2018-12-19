8ae971792d6af4e1693f370285d5a29411e8482b
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-19 18:50:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+// let companyName = 'Scuber';
+
+const companyName = 'Scuber'
+  function typeError() {
+    if (companyName != ('Scuber')) {
+      throw ("TypeError");
+    }
+  }
+
+var mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
