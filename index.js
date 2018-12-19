0e2264fae80ca03cb3b4b7a2e09a8186e3ccd0f3
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-19 18:47:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 
+const compnayName = 'Scuber2'
   function typeError() {
     if (companyName != ('Scuber')) {
       throw ("TypeError");
