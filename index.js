207d0dbb4265e0dc10d15efcf673f2eb45fd0bc0
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-19 18:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 
+function upperCase(name) {
+  if (typeof name !== "string") {
+    throw TypeError("name must be a string");
+  }
+  return name.toUpperCase();
+}
